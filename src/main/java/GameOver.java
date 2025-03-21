fa83f44cb317408e9e34c3b11ecdf6b4fb10d5dc
MH Taherii <Tari.tahery@gmail.com>
2025-03-22 00:38:52 +0330

    update

--- src/main/java/GameOver.java
+++ src/main/java/GameOver.java
@@ -2,7 +2,6 @@ public class GameOver {
     public static boolean isGameOver(char[][] grid1, char[][] grid2) {
         return allShipsSunk(grid1) || allShipsSunk(grid2);
     }
-
     public static boolean allShipsSunk(char[][] grid) {
         for (int i = 0; i < Board.GRID_SIZE; i++) {
             for (int j = 0; j < Board.GRID_SIZE; j++) {
